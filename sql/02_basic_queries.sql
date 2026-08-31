65067c71148948eb31f6ab3f928a6726ac8338c4
ekevgendm <ekevgendm@gmail.com>
2026-08-31 17:24:37 +0300

    Update 02_basic_queries.sql

--- sql/02_basic_queries.sql
+++ sql/02_basic_queries.sql
@@ -1,6 +1,12 @@
+-- Basic queries
+
+-- All employees
+
 SELECT *
 FROM employees;
 
+
+-- Employees with salary above 100000
 
 SELECT name, salary
 FROM employees
@@ -8,11 +14,15 @@ WHERE salary > 100000
 ORDER BY salary DESC;
 
 
+-- Number of employees by department
+
 SELECT department_id, COUNT(*) AS employee_count
 FROM employees
 GROUP BY department_id
 ORDER BY employee_count DESC;
 
+
+-- Departments with average salary above 90000
 
 SELECT department_id, AVG(salary) AS average_salary
 FROM employees
